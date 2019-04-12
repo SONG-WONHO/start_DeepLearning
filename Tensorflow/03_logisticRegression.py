2e96df490c020a7b786f1692b7cba0d398c3adfe
SONG-WONHO <win778500@gmail.com>
2019-04-13 02:57:05 +0900

    Update 03_logisticRegression.py

--- Tensorflow/03_logisticRegression.py
+++ Tensorflow/03_logisticRegression.py
@@ -1,27 +1,27 @@
 import tensorflow as tf
 import numpy as np
 
-#load data
+# load data
 samples = np.loadtxt("./samples/diabetes.csv", dtype=np.float32, delimiter=',')
 
-#constants
-number_feature = len(samples[0]) -1
+# constants
+number_feature = len(samples[0]) - 1
 learning_rate = 0.01
 
-#nomalize data
+# nomalize data
 for i in range(number_feature):
-    samples[:,i] = (samples[:,i]-samples[:,i].mean())/samples[:,i].std()
+    samples[:, i] = (samples[:, i] - samples[:, i].mean()) / samples[:, i].std()
 
-#separate train, test
+# separate train, test
 train_size = int(len(samples) * 0.7)
 
 train = samples[:train_size]
-x_train = train[:,:-1]
-y_train = train[:,[-1]]
+x_train = train[:, :-1]
+y_train = train[:, [-1]]
 
 test = samples[train_size:]
-x_test = test[:,:-1]
-y_test = test[:,[-1]]
+x_test = test[:, :-1]
+y_test = test[:, [-1]]
 
 X = tf.placeholder(tf.float32, [None, number_feature])
 Y = tf.placeholder(tf.float32, [None, 1])
@@ -32,30 +32,30 @@ b = tf.Variable(tf.random_normal([1]), name="bias")
 logits = tf.matmul(X, W) + b
 hypothesis = tf.nn.sigmoid(logits)
 
-#logistic cost function
-cost = -tf.reduce_mean(Y*tf.log(hypothesis)+(1-Y)*tf.log(1-hypothesis))
+# logistic cost function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 train = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-#verifiction
+# verification
 prediction = tf.cast(hypothesis > 0.5, tf.float32, "prediction")
 accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, Y), tf.float32, "accuracy"))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    #train
+    # train
     for step in range(20000):
-        cost_val, accuracy_val, _ = sess.run([cost, accuracy, train], feed_dict={X:x_train, Y:y_train})
+        cost_val, accuracy_val, _ = sess.run([cost, accuracy, train], feed_dict={X: x_train, Y: y_train})
 
         if step % 2000 == 0:
-            print("[{}] cost {} accuracy {}".format(step,cost_val, accuracy_val))
+            print("[{}] cost {} accuracy {}".format(step, cost_val, accuracy_val))
 
-    #test
+    # test
     print("***Test data***")
-    for p,y in zip(sess.run(prediction,feed_dict={X:x_test, Y:y_test}), y_test):
+    for p, y in zip(sess.run(prediction, feed_dict={X: x_test, Y: y_test}), y_test):
         print("Prediction {} True_Y {}".format(p, y))
 
-    print("accuracy {}".format(sess.run(accuracy,feed_dict={X:x_test,Y:y_test})))
+    print("accuracy {}".format(sess.run(accuracy, feed_dict={X: x_test, Y: y_test})))
 
 """
 ***Train***
